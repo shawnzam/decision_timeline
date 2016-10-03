5e235086f5bb99665a167a2dd2ab3c5a7fdbd998
Prelang Builder <builder@prelang.com>
2016-10-03 16:01:00 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'DecisionBlock' to allow for create, update

--- app/admin/decision_block.rb
+++ app/admin/decision_block.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register DecisionBlock do
+  permit_params :decision_text, :value, :ordering, :type
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
