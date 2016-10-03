d20a28d26c574f1273cfa859abf5f3e29b4b1dc8
Prelang Builder <builder@prelang.com>
2016-10-03 16:00:51 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'SubjectDecisions' to allow for create, update

--- app/admin/subject_decisions.rb
+++ app/admin/subject_decisions.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register SubjectDecisions do
+  permit_params :choice_value
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
